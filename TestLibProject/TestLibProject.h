96d87c0c3e7f78864f11bb9e64cb05803f6a1f5e
Alexander Prokofiev <alexx.prokofiev@gmail.com>
2013-10-02 12:05:02 -0400

    project commit

---
+++ TestLibProject/TestLibProject.h
@@ -0,0 +1,13 @@
+//
+//  TestLibProject.h
+//  TestLibProject
+//
+//  Created by Alexander Prokofiev on 10/1/13.
+//  Copyright (c) 2013 home. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestLibProject : NSObject
+- (void)doSomething;
+@end
---
+++ TestLibProject/TestLibProject.m
@@ -0,0 +1,15 @@
+//
+//  TestLibProject.m
+//  TestLibProject
+//
+//  Created by Alexander Prokofiev on 10/1/13.
+//  Copyright (c) 2013 home. All rights reserved.
+//
+
+#import "TestLibProject.h"
+
+@implementation TestLibProject
+- (void)doSomething {
+    NSLog(@"Just did something ...");
+}
+@end
